7c01745ea243e93e843e0bec5504400bd325c6d6
Дмитрий Татьянченко <Dmitriy@MacBook-Air-Dmitrij-4.local>
2020-09-15 20:31:08 +0300

    homework

--- module1/HW_3/task14.1/Program.cs
+++ module1/HW_3/task14.1/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Татьянченко Д.Д.
-//Семинар 3 задача 14.2
+//Семинар 3 задача 14.1
 //Обрабатывает ошибки ввода, перезапускается из терминала при невалидном вводе.
 
 using System;
@@ -8,21 +8,26 @@ namespace task14
 {
     class MainClass
     {
-        public static double Method(double x, double y)
+        public static bool Method(double x, double y)
         /*
-         * Метод для задачи 1, решающий поставленную задачу через условный оператор.           
+         * Метод для решения задачи 2.
+         * 1.Принадлежит ли точка кругу? - проверка длины радиус-вектора
+         * 2.Принадлежит ли точка заданному сектору? - проверка тангенса
+         * (оставляем только часть круга, принадлежащюю 1й и 4й четвертям, 
+         * тогда арктангенс определен однозначно) 
+         * При делении на 0(x = 0) у/х принимает значение Double.PositiveInfinity
+         * ошибки не возникает + истинность выражения не нарушается.       
          */
         {
-            if ((x < y) && (x > 0)) return x + Math.Sin(y);
-            else if ((x > y) && (x < 0)) return x + Math.Sin(y);
-            else return 0.5 * x * y;
+            return x * x + y * y <= 4 && x >= 0 ?
+            (y / x <= 1 && y / x >= Double.NegativeInfinity ? true : false) : false;
         }
         public static void Main(string[] args)
         {
         Inp: Console.WriteLine("Enter x and y:");
             double x, y;
             if (double.TryParse(Console.ReadLine(), out x) &&
-                      double.TryParse(Console.ReadLine(), out y))
+                     double.TryParse(Console.ReadLine(), out y))
             {
                 Console.WriteLine($"Ans: {Method(x, y)}");
             }
--- module1/HW_3/task14.1/Properties/AssemblyInfo.cs
+++ module1/HW_3/task14.1/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("task14.1")]
+[assembly: AssemblyTitle("task14_2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
