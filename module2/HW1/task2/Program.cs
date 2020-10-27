6b9f4c72d7eb53249015ae91c6a0a4715010c336
Дмитрий Татьянченко <Dmitriy@MacBook-Air-Dmitrij-4.local>
2020-10-28 01:48:05 +0300

    hw1 module 2 done

---
+++ module2/HW1/task2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ module2/HW1/task7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task7
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
