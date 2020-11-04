72f1cc016c1338bbc4ccddcce32bce55236f982d
Дмитрий Татьянченко <Dmitriy@MacBook-Air-Dmitrij-4.local>
2020-11-04 17:39:43 +0300

    homework upd: 04.11.2020

---
+++ module2/HW1/task3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace task3
+{
+    class Polygon
+    {
+        // Fields.
+        int edges;
+        double radius;
+
+        // Properties.
+        private int Perimetr
+        {
+            get; set;
+        }
+        private double Area
+        {
+            get; set;
+        }
+
+        // Constructor.
+        public Polygon(int edges = 0, double radius = 0, int Perimetr = 0, double Area = 0)
+        {
+            this.edges = edges;
+            this.radius = radius;
+            this.Perimetr = Perimetr;
+            this.Area = Area;
+        }
+
+        // Method.
+        public string PolygonData()
+        {
+            return $"Edges: {edges}, radius: {radius}, perimetr: {Perimetr}, area: {Area}";
+        }
+
+    }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Polygon poly = new Polygon(1, 1, 1, 1);
+            Console.WriteLine(poly.PolygonData());
+        }
+    }
+}
---
+++ module2/HW2/auf_style_95/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+namespace task3
+{
+    // Да это не по теме задания, но я так кайфую от того, что смог на шарпе руками реализовать 
+    // за 70+ строк то, что в питоне пишется в 1 строчку, что я просто с кайфом оставлю это здесь /\ ауф \/
+
+    class IntArrayComparer : IComparer<int[]>
+    {
+        int index;
+        int reverse;
+
+        int Index
+        {
+            get
+            {
+                return index;
+            }
+            set
+            {
+                if (value >= 0) index = value;
+                else throw new ArgumentException("Negative index.");
+            }
+        }
+        int Reverse
+        {
+            get
+            {
+                return reverse;
+             }
+            set
+            {
+                if (value == 0) reverse = 1;
+                if (value == 1) reverse = -1;
+            }
+        }
+
+        public IntArrayComparer(int i, bool reverse = false)
+        {
+            Index = i;
+            Reverse = Convert.ToInt32(reverse);
+        }
+
+        public int Compare(int[] a1, int[] a2)
+        {
+            if (a1[Index] > a2[Index]) return reverse;
+            else if (a1[Index] < a2[Index]) return -reverse;
+            else return 0;
+        }
+    }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[][] arr = new int[4][];
+            arr[0] = new int[4] { 1, 2, 3, 4 };
+            arr[1] = new int[4] { 1, 0, 3, 4 };
+            arr[2] = new int[4] { 1, 100, 3, 4 };
+            arr[3] = new int[4] { 1, 6, 3, 4 };
+
+            Array.Sort(arr, new IntArrayComparer(1));
+
+            foreach (int[] i in arr)
+            {
+                foreach (int j in i)
+                {
+                    Console.Write($"{j} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
