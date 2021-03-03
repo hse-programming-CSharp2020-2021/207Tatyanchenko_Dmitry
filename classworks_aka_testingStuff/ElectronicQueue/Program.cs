845b743055ccb84265fa49cfd8466e1ef8d78076
Дмитрий Татьянченко <main@MacBook-Pro-Dmitrij.local>
2021-03-03 10:02:34 +0300

    upd 03.02.21

---
+++ classworks_aka_testingStuff/ElectronicQueue/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ElectronicQueue
+{
+    struct Person
+    {
+        int Age { get; set; }
+        string Name { get; set; }
+        string Surname { get; set; }
+
+        public Person(int age, string name, string surname)
+        {
+            this.Age = age;
+            Name = name;
+            Surname = surname;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Surname: {Surname}, Age: {Age}.";
+        }
+    }
+
+    class ElectronicQueue
+    {
+        public Queue<Person> elq = new Queue<Person>();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            ElectronicQueue elq = new ElectronicQueue();
+            Console.WriteLine("enqueueing:");
+            for (int i = 0; i < 5; i++)
+            {
+                Person p = new Person(rnd.Next(0, 25), rnd.Next(0, 25).ToString(), rnd.Next(0, 25).ToString());
+                elq.elq.Enqueue(p);
+                Console.WriteLine(p);
+            }
+            Console.WriteLine("dequeueing:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(elq.elq.Dequeue());
+            }
+        }
+    }
+}
---
+++ classworks_aka_testingStuff/IFigure/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace IFigure
+{
+    interface IFigure
+    {
+        // returns area of the figure.
+        public double Area();
+    }
+
+    class Square : IFigure
+    {
+        public double Length { get; set; }
+
+        public Square(double length)
+        {
+            this.Length = length;
+        }
+
+        public double Area()
+        {
+            return Length * Length;
+        }
+    }
+
+    class Circle : IFigure
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double Area()
+        {
+            return Radius * Radius;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void ExtraordinaryArea<T>(T[] figures, double threshold) where T:IFigure
+            {
+                foreach(IFigure figure in figures)
+                {
+                    if (figure.Area() > threshold) Console.WriteLine($"Big area: {figure.Area()}");
+                }
+            }
+
+            Random rnd = new Random();
+
+            IFigure[] figures = new IFigure[10];
+            for (int i = 0; i < 5; i++)
+            {
+                figures[i] = new Square(rnd.Next(0,100));
+                Console.WriteLine(figures[i].Area());
+            }
+            for (int i = 5; i < 10; i++)
+            {
+                figures[i] = new Circle(rnd.Next(0, 100));
+                Console.WriteLine(figures[i].Area());
+            }
+
+            ExtraordinaryArea<IFigure>(figures, 20);
+
+
+        }
+    }
+}
